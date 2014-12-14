b0415115873f0430b4c7b064021734525c39d059
Thulio Philipe <thulioph@gmail.com>
2014-12-14 20:11:25 -0300

    Adiciona iscroll

--- _scripts.php
+++ _scripts.php
@@ -3,9 +3,10 @@
   <script src="dist/js/libs.min.js"></script>
 
   <script src="src/js/APP.js"></script>
+  <script src="src/js/APP.Status.js"></script>
+  <script src="src/js/APP.Iscroll.js"></script>
   <script src="src/js/APP.General.js"></script>
   <script src="src/js/APP.Redirect.js"></script>
-  <script src="src/js/APP.Status.js"></script>
   <script>APP.init();</script>
 
 </body>
