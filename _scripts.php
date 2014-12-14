6678396bed22c327adf0d7026eaca6a69806d9fd
Thulio Philipe <thulioph@gmail.com>
2014-12-14 15:11:59 -0300

    Adiciona includes - Fixed #4

---
+++ _scripts.php
@@ -0,0 +1,10 @@
+<!-- JS -->
+  <!-- // <script src="dist/js/scripts.min.js"></script> -->
+  <script src="dist/js/libs.min.js"></script>
+
+  <script src="src/js/APP.js"></script>
+  <script src="src/js/APP.Redirect.js"></script>
+  <script>APP.init();</script>
+
+</body>
+</html>
