25fe313238cc314b7d407f1c4d4bf67bc07e6efe
Leonardo Paiva <leonardo@pianolab.com.br>
2014-12-15 00:49:31 -0200

    listando receitas por categoria - aguardando tela pra finalizar

--- _scripts.php
+++ _scripts.php
@@ -6,6 +6,9 @@
   <script src="src/js/APP.General.js"></script>
   <script src="src/js/APP.Redirect.js"></script>
   <script src="src/js/APP.Status.js"></script>
+  <script src="src/js/APP.Storage.js"></script>
+  <script src="src/js/modules/APP.Recipe.js"></script>
+
   <script>APP.init();</script>
 
 </body>
---
+++ src/js/APP.Storage.js
@@ -0,0 +1,50 @@
+/*======================================
+App Storage Controler
+======================================*/
+var APP = APP || {};
+
+APP.Storage = {
+    init: function(){
+
+    },
+
+    getRecipeByCategory: function(url,success){
+      this.loadAjax(url,success);
+    },
+
+    //AJAX
+    loadAjax: function(url,success) {
+      var that = this;
+
+      $.ajax({
+        type: "POST",
+        url: url,
+        //async: false,
+        data: {},
+        dataType: 'json',
+        beforeSend: function() {
+          that.loading.apply(that, arguments);
+        },
+        success: function(data, text, xhr) {
+          console.log('load complete')
+          success(arguments[0]);
+        },
+        error: function() {
+          that.loadError.apply(that, arguments);
+        }
+      });
+    },
+
+    loading: function(){
+      console.log('loading');
+    },
+
+    loadComplete: function(){
+      console.log('load complete');
+    },
+
+    loadError: function(){
+      console.log('erro');
+    }
+    //END AJAX
+}
