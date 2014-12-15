b74d0c4a6778521e593f449f13e53714c315b729
Thulio Philipe <thulioph@gmail.com>
2014-12-15 01:37:56 -0300

    Adiciona include do header

---
+++ _header.php
@@ -0,0 +1,45 @@
+<header id="header-primary" class="header-primary">
+  <a href="#categories" class="link-global">
+    <span class="glyphicon glyphicon-list"></span>
+  </a>
+
+  <a href="#config" class="link-global">
+    <span class="glyphicon glyphicon-user"></span>
+  </a>
+</header>
+
+<!-- offcanvas left -->
+<aside id="categories" class="off-canvas categories">
+  <h3 class="greeting text-info">Escolha alguma categoria.</h3>
+  <input type="search" class="input-search form-control" placeholder="Busque alguma categoria digitando.">
+
+  <ul id="categories-list" class="categories-list">
+    <li class="categories-item"><a href="recipes/aves.html" class="categories-link">Aves</a></li>
+    <li class="categories-item"><a href="#" class="categories-link">Bebidas</a></li>
+    <li class="categories-item"><a href="#" class="categories-link">Bolos</a></li>
+    <li class="categories-item"><a href="#" class="categories-link">Carnes</a></li>
+    <li class="categories-item"><a href="#" class="categories-link">Doces</a></li>
+    <li class="categories-item"><a href="#" class="categories-link">Especiais</a></li>
+    <li class="categories-item"><a href="#" class="categories-link">Lanches</a></li>
+    <li class="categories-item"><a href="#" class="categories-link">Massas</a></li>
+    <li class="categories-item"><a href="#" class="categories-link">Peixes e Frutos do Mar</a></li>
+    <li class="categories-item"><a href="#" class="categories-link">Prato Único</a></li>
+    <li class="categories-item"><a href="#" class="categories-link">Saladas</a></li>
+    <li class="categories-item"><a href="#" class="categories-link">Sopas</a></li>
+  </ul>
+</aside>
+
+<!-- offcanvas right -->
+<aside id="config" class="off-canvas config">
+  <h3 class="greeting text-info">Seu perfil.</h3>
+
+  <ul class="config-list">
+    <li class="config-item"><a href="#" class="config-link">Adicionar Receita</a></li>
+    <li class="config-item"><a href="#" class="config-link">Minhas Receitas</a></li>
+    <li class="config-item"><a href="#" class="config-link">Receitas Favoritas</a></li>
+    <li class="config-item"><a href="#" class="config-link">Receitas Aprovadas</a></li>
+    <li class="config-item"><a href="#" class="config-link">Receitas Avaliadas</a></li>
+    <li class="config-item"><a href="#" class="config-link">Alterar Senha</a></li>
+    <li class="config-item"><a href="index" class="config-link">Sair</a></li>
+  </ul>
+</aside>
