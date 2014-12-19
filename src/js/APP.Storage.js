b2f5a4e627166939672cd7b301d1b9b714421048
Thulio Philipe <thulioph@gmail.com>
2014-12-19 02:17:21 -0300

    Calcula o tamanho do localStorage e mostra no console

--- src/js/APP.Storage.js
+++ src/js/APP.Storage.js
@@ -24,11 +24,28 @@ APP.Storage = {
 
   // verifica se o conteúdo existe;
   getStorageReceitasInterna: function(key, href) {
+    var that = this;
+
     if (localStorage.getItem(key) == null) {
       APP.Request.ReceitasInterna.ajax(href, key);
     } else {
+      that.checkCapaticy(key);
       var href = JSON.parse(localStorage.getItem(key));
       $('#main').html(href);
     }
+  },
+
+  // verifica a capacidade do localStorage
+  checkCapaticy: function(key) {
+    var len, max;
+
+      max = 5126,
+      len = localStorage.getItem(key).length;
+
+      if (len <= max) {
+        console.warn('Seu localStorage está com: ' +  Math.round(max-len) + 'kb');
+      } else {
+        console.warn('Seu localStorage está com: ' + len);
+      }
   }
 }
