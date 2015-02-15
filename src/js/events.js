7324ce04a96453945df1ef296a61a722c88995c3
Thulio Philipe <thulioph@gmail.com>
2015-02-15 00:31:11 -0300

    Adiciona algumas coisas.

--- src/js/events.js
+++ src/js/events.js
@@ -5,3 +5,19 @@ function shortToast() {
   });
 }
 
+function dialog() {
+  new fries.Toast({
+    content: 'Sua receita foi adicionada.',
+    duration: fries.Toast.duration.LONG
+  });
+
+  window.setTimeout(redirect, 3000);
+
+  function redirect() {
+    var page = 'adicionar-receita/'
+    var href = window.location.href = 'http://localhost:8888/unibratec-pos/projeto-final/prove-aprove/#/user/';
+
+    href.replace(page);
+  }
+}
+
